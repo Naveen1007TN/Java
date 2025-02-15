d2e5c1af6e50cacc01bff4c70e7d4ca97c572240
Naveen1007TN <Naveentemp1007@gmail.com>
2025-02-15 18:42:02 +0530

    new

---
+++ base.java
@@ -0,0 +1,19 @@
+class base{
+    public static void main(String[] args) {
+        // data type
+        int num = 12; // int is a datatype and num is Variable user defined and you declare as empty or insert a value
+        String name = "Naveen";
+        byte value = 127; // in byte we declare the value upto 127
+        char single = 'A'; // we define a value with double quotation
+        long bigint = 128l; // we need initialize l at finishing
+        boolean tbool = true; // in boolean without declaration we can't print the boolean
+
+        System.err.println(tbool);
+        System.err.println(num);
+        System.err.println(name);
+        System.err.println(value);
+        System.err.println(single);
+        System.err.println(bigint);
+
+    }
+}
